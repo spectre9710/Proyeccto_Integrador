13c58702c69032d30b56de7b17a5f79a2ad4ca58
spectre9710 <ramialan176@gmail.com>
2024-03-24 21:49:04 -0600

    eliminacion de musicas

--- src/db/music.js
+++ src/db/music.js
@@ -1,150 +1,6 @@
 let musicDB = [
   {
     id: 0,
-    name: "Shakedown",
-    author_name: "Clarx",
-    img: "shakedown.jpg",
-    lang: "ENGLISH",
-    timesPlayed: 0,
-    type: "electronic",
-    musicName: "Clarx - Shakedown [NCS Release].mp3",
-    attribution: {
-      song: "Clarx - Shakedown [NCS Release]",
-      musicBy: "NoCopyrightSounds",
-      download: "http://ncs.io/Shakedown",
-      stream: "http://youtu.be/qJT0mc3q6Lg"
-    }
-  },
-  {
-    id: 1,
-    name: "Games Worldbeat",
-    author_name: "Bernardo R.",
-    img: "notAvailable.jpg",
-    lang: null,
-    timesPlayed: 0,
-    type: "instrumental",
-    musicName: "mixkit-games-worldbeat-466.mp3",
-    attribution: {
-      song: "Games Worldbeat",
-      musicBy: "mixkit",
-      download: null,
-      stream: null
-    }
-  },
-  {
-    id: 2,
-    name: "OverMyHead",
-    author_name: " ROY KNOX",
-    img: "over-my-head-1602154826-mOiKzOJDe6.jpg",
-    lang: "ENGLISH",
-    type: "electronic",
-    timesPlayed: 0,
-    musicName: "ROY KNOX -  Over My Head (Feat. Mike Robert) [NCS Release].mp3",
-    attribution: {
-      song: "ROY KNOX - Over My Head (Feat. Mike Robert) [NCS Release]",
-      musicBy: "NoCopyrightSounds",
-      download: "http://ncs.io/OverMyHead",
-      stream: null
-    }
-  },
-  {
-    id: 3,
-    name: "Want You (feat. Sara Skinner)",
-    author_name: "Subtact, Sara Skinner",
-    img: "want-you-feat-sara-skinner-1586956276-s4lOrfrHQj.jpg",
-    lang: "ENGLISH",
-    timesPlayed: 0,
-    type: "electronic",
-    musicName: "Subtact - Want You (feat. Sara Skinner) [NCS Release].mp3",
-    attribution: {
-      song: "Subtact - Want You (feat. Sara Skinner) [NCS Release]",
-      musicBy: "NoCopyrightSounds",
-      download: "http://ncs.io/WantYou",
-      stream: "http://youtu.be/Pww31vN_1QY"
-    }
-  },
-  {
-    id: 4,
-    name: "Apocalypse",
-    author_name: "Midranger",
-    img: "apocalypse-1600776027-tsCmgK4gEU.jpg",
-    lang: "ENGLISH",
-    timesPlayed: 0,
-    type: "electronic",
-    musicName: "Midranger - Apocalypse [NCS Release].mp3",
-    attribution: {
-      song: "Midranger - Apocalypse [NCS Release]",
-      musicBy: "NoCopyrightSounds",
-      download: "http://ncs.io/Apocalypse",
-      stream: "http://youtu.be/Whw1sWDpAvE"
-    }
-  },
-  {
-    id: 5,
-    name: "Alive",
-    author_name: "Neoni, N3WPORT",
-    img: "alive.jpg",
-    lang: "ENGLISH",
-    timesPlayed: 0,
-    type: "electronic",
-    musicName: "N3WPORT - Alive (feat. Neoni) [NCS Release].mp3",
-    attribution: {
-      song: "N3WPORT - Alive (feat. Neoni) [NCS Release]",
-      musicBy: "NoCopyrightSounds",
-      download: "http://ncs.io/NAlive",
-      stream: "http://youtu.be/vJAcXd_UtWQ"
-    }
-  },
-  {
-    id: 6,
-    name: "Impact Moderato",
-    author_name: "Kevin MacLeod",
-    img: "notAvailable.jpg",
-    lang: "ENGLISH",
-    timesPlayed: 0,
-    type: "instrumental",
-    musicName: "impact.mp3",
-    attribution: {
-      song: "Impact Moderato",
-      musicBy: "Kevin MacLeod",
-      download: null,
-      stream: null
-    }
-  },
-  {
-    id: 7,
-    name: "Impact Moderato 2",
-    author_name: "Kevin MacLeod",
-    img: "notAvailable.jpg",
-    lang: "ENGLISH",
-    timesPlayed: 0,
-    type: "instrumental",
-    musicName: "impact2.mp3",
-    attribution: {
-      song: "Impact Moderato",
-      musicBy: "Kevin MacLeod",
-      download: null,
-      stream: null
-    }
-  },
-  {
-    id: 8,
-    name: "Ignite",
-    author_name: "Tom wilson, Heuse",
-    img: "ignite-1600419626-nHn4WXY79q.jpg",
-    lang: "ENGLISH",
-    timesPlayed: 0,
-    type: "electronic",
-    musicName: "Heuse & Tom Wilson - Ignite [NCS Release].mp3",
-    attribution: {
-      song: "Heuse & Tom Wilson - Ignite [NCS Release]",
-      musicBy: "NoCopyrightSounds",
-      download: "http://ncs.io/Ignite",
-      stream: "http://youtu.be/zbqgv0mMfwk"
-    }
-  },
-  {
-    id: 9,
     name: "Goteo",
     author_name: "Duki",
     img: "Goteo.jpg",
@@ -152,15 +8,9 @@ let musicDB = [
     timesPlayed: 0,
     type: "trap",
     musicName: "Goteo  - Duki.mp3",
-    attribution: {
-      song: "Goteo  - Duki",
-      musicBy: "Duki",
-      download: "https://www.youtube.com/watch?v=FRthkpJ_NFo",
-      stream: "https://www.youtube.com/@duki"
-    }
   },
   {
-    id: 10,
+    id: 1,
     name: "PERRO NEGRO",
     author_name: "Bad Bunny ft. Feid",
     img: "Nadie sabe lo que va a pasar mañana.png",
@@ -168,15 +18,9 @@ let musicDB = [
     timesPlayed: 0,
     type: "regueton",
     musicName: "Perro Negro - Bad Bunny.mp3",
-    attribution: {
-      song: "Perro Negro - Bad Bunny",
-      musicBy: "Bad Bunny",
-      download: "https://www.youtube.com/watch?v=O2lC_oBI7DU",
-      stream: "https://www.youtube.com/channel/UCmBA_wu8xGg1OfOkfW13Q0Q"
-    }
   },
   {
-    id: 11,
+    id: 2,
     name: "Sunflower (Spider-Man: Into the Spider-Verse)",
     author_name: "Post Malone, Swae Lee",
     img: "Sunflower.jpg",
@@ -184,15 +28,9 @@ let musicDB = [
     timesPlayed: 0,
     type: "trap",
     musicName: "Sunflower - Post Malone.mp3",
-    attribution: {
-      song: "Sunflower - Post Malone",
-      musicBy: "Post Malone",
-      download: "https://www.youtube.com/watch?v=ApXoWvfEYVU",
-      stream: "https://www.youtube.com/channel/UCeLHszkByNZtPKcaVXOCOQQ"
-    }
-  }, 
+  },
   {
-    id: 12,
+    id: 3,
     name: "Tal vez",
     author_name: "Paulo Londra",
     img: "Homerun.jpeg",
@@ -200,15 +38,9 @@ let musicDB = [
     timesPlayed: 0,
     type: "trap",
     musicName: "Tal Vez - Paulo Londra.mp3",
-    attribution: {
-      song: "Tal Vez - Paulo Londra",
-      musicBy: "Paulo Londra",
-      download: "https://www.youtube.com/watch?v=NPpELzyP4rw",
-      stream: "https://www.youtube.com/channel/UCzI7wZyqk7jpXWT_MILjepw"
-    }
   },
   {
-    id: 13,
+    id: 4,
     name: "Me Porto Bonito",
     author_name: "Bad Bunny ft. Chencho Corleone",
     img: "Un Verano Sin Ti.jpg",
@@ -216,15 +48,9 @@ let musicDB = [
     timesPlayed: 0,
     type: "regueton",
     musicName: "Me Porto Bonito - Bad Bunny.mp3",
-    attribution: {
-      song: "Me Porto Bonito - Bad Bunny",
-      musicBy: "Bad Bunny",
-      download: "https://www.youtube.com/watch?v=saGYMhApaH8",
-      stream: "https://www.youtube.com/channel/UCmBA_wu8xGg1OfOkfW13Q0Q"
-    }
   },
   {
-    id: 14,
+    id: 5,
     name: "Azul",
     author_name: "J Balvin",
     img: "Colores.jpeg",
@@ -232,15 +58,9 @@ let musicDB = [
     timesPlayed: 0,
     type: "regueton",
     musicName: "Azul - J Balvin.mp3",
-    attribution: {
-      song: "Azul - J Balvin",
-      musicBy: "J Balvin",
-      download: "https://www.youtube.com/watch?v=bcaLBKH-Yfc",
-      stream: "https://www.youtube.com/@jbalvinVEVO"
-    }
   },
   {
-    id: 15,
+    id: 6,
     name: "La Rompe Corazones",
     author_name: "Daddy Yankee ft. Ozuna",
     img: "La Rompe Corazones.jpg",
@@ -248,15 +68,9 @@ let musicDB = [
     timesPlayed: 0,
     type: "regueton",
     musicName: "La Rompe Corazones  - Daddy Yankee.mp3",
-    attribution: {
-      song: "La Rompe Corazones  - Daddy Yankee",
-      musicBy: "Daddy Yankee",
-      download: "https://www.youtube.com/watch?v=ks1CBTwtaU8&rco=1",
-      stream: "https://www.youtube.com/@DaddyYankee"
-    }
   },
   {
-    id: 16,
+    id: 7,
     name: "Despacito",
     author_name: "Luis Fonsi ft. Daddy Yankee",
     img: "Despacito.avif",
@@ -264,15 +78,9 @@ let musicDB = [
     timesPlayed: 0,
     type: "regueton",
     musicName: "Despacito - Luis Fonsi.mp3",
-    attribution: {
-      song: "Despacito - Luis Fonsi",
-      musicBy: "Luis Fonsi",
-      download: "https://www.youtube.com/watch?v=kJQP7kiw5Fk",
-      stream: "https://www.youtube.com/channel/UCxoq-PAQeAdk_zyg8YS0JqA"
-    }
   },
   {
-    id: 17,
+    id: 8,
     name: "Relación Remix",
     author_name: "Sech, Daddy Yankee, J Balvin, Rosalía, Farruko",
     img: "Relación Remix.jpeg",
@@ -280,15 +88,9 @@ let musicDB = [
     timesPlayed: 0,
     type: "regueton",
     musicName: "Relación Remix - Sech.mp3",
-    attribution: {
-      song: "Relación Remix - Sech",
-      musicBy: "Sech",
-      download: "https://www.youtube.com/watch?v=XseIJg8Vyj0",
-      stream: "https://www.youtube.com/channel/UCte53PewI8_jYpBYn071MqQ"
-    }
   },
   {
-    id: 18,
+    id: 9,
     name: "Te Boté - Remix",
     author_name: "Nio García, Casper, Darell, Nicky Jam, Bad Bunny, Ozuna",
     img: "Now Or Never.webp",
@@ -296,15 +98,9 @@ let musicDB = [
     timesPlayed: 0,
     type: "trap",
     musicName: "Te Boté Remix - Nio Garcia.mp3",
-    attribution: {
-      song: "Te Boté Remix - Nio Garcia",
-      musicBy: "Nio García",
-      download: "https://www.youtube.com/watch?v=9jI-z9QN6g8",
-      stream: "https://www.youtube.com/@FlowLaMovie"
-    }
   },
   {
-  id: 19,
+    id: 10,
     name: "Nathy Peluso: Bzrp Music Sessions, Vol.36",
     author_name: "Bizarrap, NATHY PELUSO",
     img: "Music Sessions, Vol.36.jpg",
@@ -312,293 +108,17 @@ let musicDB = [
     timesPlayed: 0,
     type: "trap",
     musicName: "BZRP Music Sessions 36.mp3",
-    attribution: {
-      song: "BZRP Music Sessions 36",
-      musicBy: "Bizarrap",
-      download: "https://www.youtube.com/watch?v=0OkiUUU3Odw",
-      stream: "https://www.youtube.com/channel/UCmS75G-98QihSusY7NfCZtw"
-    }
   },
   {
-    id: 20,
-      name: "Circles",
-      author_name: "Post Malone",
-      img: "Hollywood's Bleeding.jpg",
-      lang: "ENGLISh",
-      timesPlayed: 0,
-      type: "pop",
-      musicName: "Circles - Post Malone.mp3",
-      attribution: {
-        song: "Circles - Post Malone",
-        musicBy: "Post Malone",
-        download: "https://www.youtube.com/watch?v=wXhTHyIgQ_U",
-        stream: "https://www.youtube.com/channel/UCeLHszkByNZtPKcaVXOCOQQ"
-      }
-    }
+    id: 11,
+    name: "Circles",
+    author_name: "Post Malone",
+    img: "Hollywood's Bleeding.jpg",
+    lang: "ENGLISh",
+    timesPlayed: 0,
+    type: "pop",
+    musicName: "Circles - Post Malone.mp3",
+  }
 ];
 
 export default musicDB;
-/*let musicDB = [
-  {
-    id:0,
-    name:"Left Foot Up Right Foot Slide",
-    author_name: "Yung Nazty",
-    img: "left_foot_up_right_foot_slide.jpg",
-    lang: "ENGLISH",
-    timesPlayed:0,
-    musicName:"left_foot_up_right_foot_slide.mp3"
-  },
-  {
-    id:1,
-    name: "Intentions (feat. Quavo)",
-    author_name: "Justin Bieber",
-    img: "intentions.jpg",
-    lang: "ENGLISH",
-    timesPlayed:0,
-    musicName:"intentions.mp3"
-  },
-  {
-    id:2,
-    name: "Naina Da Kya Kasoor",
-    author_name: "Amit Trivedi",
-    img: "naina_da_kya_kasoor.jpg",
-    lang:"HINDI",
-    timesPlayed:0,
-    musicName:"Naina_da_kya_kasoor.mp3"
-  },
-  {
-    id:3,
-    name: "Señorita",
-    author_name: "Shawn Mendes",
-    img: "senorita.jpg",
-    lang: "ENGLISH",
-    timesPlayed:0,
-    musicName:"senorita.mp3"
-  },
-  {
-    id:4,
-    name: "Memories",
-    author_name: "Maroon 5",
-    img: "memories.jpg",
-    lang: "ENGLISH",
-    timesPlayed:0,
-    musicName:"memories.mp3"
-  },
-  {
-    id:5,
-    name: "Skechers",
-    author_name: "DripReport",
-    img: "skechers.jpg",
-    lang: "ENGLISH",
-    timesPlayed:0,
-    musicName:"Skechers.mp3"
-  },
-  {
-    id:6,
-    name: "Sunflower - Spider-Man: Into the Spider-Verse",
-    author_name: "Post Malone",
-    img: "sunflower.jpg",
-    lang: "ENGLISH",
-    timesPlayed:0,
-    musicName:"Sunflower.mp3"
-  },
-  {
-    id:7,
-    name: "I Can’t Get Enough",
-    author_name: "benny blanco, Selena Gomez, J Balvin, Tainy",
-    img: "i_cant_get_enough.jpg",
-    lang: "ENGLISH",
-    timesPlayed:0,
-    musicName:"i_cant_get_enough.mp3"
-  },
-  {
-    id:8,
-    name: "Old Town Road",
-    author_name: "Lil Nas X",
-    img: "old_town_road.jpg",
-    lang: "ENGLISH",
-    timesPlayed:0,
-    musicName:"old_town_road.mp3"
-  },
-  {
-    id:9,
-    name: "Beautiful Stillness",
-    author_name: "Billy-Joe",
-    img: "beautiful_stillness.jpg",
-    lang: "ENGLISH",
-    timesPlayed:0,
-    musicName:"Beautiful_Stillness.mp3"
-  },
-  {
-    id:10,
-    name: "death bed (coffee for your head)",
-    author_name: "Powfu",
-    img: "death_bed.jpg",
-    lang: "ENGLISH",
-    timesPlayed:0,
-    musicName:"death_bed.mp3"
-  },
-  {
-    id:11,
-    name: "Halka Halka (From \"Fanney Khan\")",
-    author_name: "Sunidhi Chauhan",
-    img: "halka_halka_suroor.jpg",
-    lang:"HINDI",
-    timesPlayed:0,
-    musicName:"halka_halka_suroor.mp3"
-  },
-  {
-    id:12,
-    name: "Kudi Nu Nachne De (From \"Angrezi Medium\")",
-    author_name: "Vishal Dadlani",
-    img: "kudi_nu_nachne_de.jpg",
-    lang:"HINDI",
-    timesPlayed:0,
-    musicName:"kudi_nu_nachne_de.mp3"
-  },
-  {
-    id:13,
-    name: "Illegal Weapon 2.0",
-    author_name: "Jasmine Sandlas",
-    img: "illeagal_weapon_2.jpg",
-    lang:"PUBJABI",
-    timesPlayed:0,
-    musicName:"illeagal_weapon_2.mp3"
-  },
-  {
-    id:14,
-    name: "Genda Phool (feat. Payal Dev)",
-    author_name: "Badshah",
-    img: "genda_phool.jpg",
-    lang:"BENGALI",
-    timesPlayed:0,
-    musicName:"genda_phool.mp3"
-  },
-  {
-    id:15,
-    name: "Yaar Mod Do",
-    author_name: "Guru Randhawa",
-    img: "yaar_mod_do.jpg",
-    lang:"PUNJABI",
-    timesPlayed:0,
-    musicName:"yaar_mod_do.mp3"
-  },
-  {
-    id:16,
-    name: "Shayad",
-    author_name: "Pritam",
-    img: "shayad.jpg",
-    lang:"HINDI",
-    timesPlayed:0,
-    musicName:"shayad.mp3"
-  },
-  {
-    id:17,
-    name: "Without Me",
-    author_name: "Halsey",
-    img: "without_me.jpg",
-    lang: "ENGLISH",
-    timesPlayed:0,
-    musicName:"without_me.mp3"
-  },
-  {
-    id:18,
-    name: "Buttabomma - Telugu",
-    author_name: "Armaan Malik",
-    img: "buttabomma.jpg",
-    lang:"TELUGU",
-    timesPlayed:0,
-    musicName:"ButtaBomma.mp3"
-  },
-  {
-    id:19,
-    name: "Coda",
-    author_name: "Jakob Ahlbom",
-    img: "coda.jpg",
-    lang: "ENGLISH",
-    timesPlayed:0,
-    musicName:"coda.mp3"
-  },
-  {
-    id:20,
-    name: "Khairiyat",
-    author_name: "Arijit Singh",
-    img: "khairiyat.jpg",
-    lang:"HINDI",
-    timesPlayed:0,
-    musicName:"khairiyat.mp3"
-  },
-  {
-    id:21,
-    name: "Haan Main Galat",
-    author_name: "Pritam",
-    img: "haan_mai_galat.jpg",
-    lang:"HINDI",
-    timesPlayed:0,
-    musicName:"haan_mai_galat.mp3"
-  },
-  {
-    id:22,
-    name: "Mere Liye Tum Kaafi Ho (From \"Shubh Mangal Zyada Saavdhan\")",
-    author_name: "Ayushmann Khurrana",
-    img: "mere_liiye_tum_kaafi_ho.jpg",
-    lang:"HINDI",
-    timesPlayed:0,
-    musicName:"mere_liiye_tum_kaafi_ho.mp3"
-  },
-  {
-    id:23,
-    name: "Fracture",
-    author_name: "Jakob Ahlbom",
-    img: "fracture.jpg",
-    lang: "ENGLISH",
-    timesPlayed:0,
-    musicName:"fracture.mp3"
-  },
-  {
-    id:24,
-    name: "Ghungroo (From \"War\")",
-    author_name: "Arijit Singh",
-    img: "ghungroo.jpg",
-    lang:"HINDI",
-    timesPlayed:0,
-    musicName:"ghungroo.mp3"
-  },
-  {
-    id:25,
-    name: "Happier",
-    author_name: "Marshmello",
-    img: "happier.jpg",
-    lang: "ENGLISH",
-    timesPlayed:0,
-    musicName:"happier.mp3"
-  },
-  {
-    id:26,
-    name: "Namo Namo",
-    author_name: "Amit Trivedi",
-    img: "namo_namo.jpg",
-    lang:"HINDI",
-    timesPlayed:0,
-    musicName:"namo_namo.mp3"
-  },
-  {
-    id:27,
-    name: "Faded",
-    author_name: "Alan Walker",
-    img: "faded.jpg",
-    lang: "ENGLISH",
-    timesPlayed:0,
-    musicName:"Faded_Alan_Walker.mp3"
-  },
-  {
-    id:28,
-    name: "I Don't Care (with Justin Bieber)",
-    author_name: "Ed Sheeran",
-    img: "i_dont_care.jpg",
-    lang: "ENGLISH",
-    timesPlayed:0,
-    musicName:"i_dont_care.mp3"
-  }
-]
-export default musicDB;*/
